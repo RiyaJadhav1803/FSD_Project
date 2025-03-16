25bf9c7e6b81883ec95c53ff46654f0c33297330
JanhviJathot03 <janhvijathot@gmail.com>
2025-03-16 21:16:33 +0530

    Applied css for all the pages

--- front/src/app/views/signup/signup.component.ts
+++ front/src/app/views/signup/signup.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
   standalone: false,
 })
 export class SignupComponent {
